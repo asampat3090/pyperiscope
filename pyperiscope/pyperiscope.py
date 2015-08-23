ad43d3bc01b120c96b47fd20a1fbdab5f471583b
Anand Sampat <asampat3090@gmail.com>
2015-08-22 23:42:01 -0700

    skeleton code

--- pyperiscope/pyperiscope.py
+++ pyperiscope/pyperiscope.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+import requests
+import re
+import json
+import os
+import subprocess as sp
+from PIL import Image
+import datetime
+
+class PeriscopeAPI(object):
+    """
+    Wrapper Class for the Periscope API
+    """
+    pass
